6fb7879a5c7bcb89503dd18cd00d015b28a020ce
yujong <yujong4170@gmail.com>
2022-12-23 00:55:08 +0900

    week2_JobQueue

--- week2_priority_queues_and_disjoint_sets_starters/job_queue/JobQueue.java
+++ week2_priority_queues_and_disjoint_sets_starters/job_queue/JobQueue.java
@@ -1,5 +1,8 @@
 import java.io.*;
 import java.util.StringTokenizer;
+import java.util.Comparator;
+import java.util.PriorityQueue;
+import java.util.Queue;
 
 public class JobQueue {
     private int numWorkers;
@@ -34,8 +37,20 @@ public class JobQueue {
         // TODO: replace this code with a faster algorithm.
         assignedWorker = new int[jobs.length];
         startTime = new long[jobs.length];
-        long[] nextFreeTime = new long[numWorkers];
-        for (int i = 0; i < jobs.length; i++) {
+        //long[] nextFreeTime = new long[numWorkers];
+        Queue<thread> q = new PriorityQueue<thread>(11,lt);
+        for (int i = 0; i < numWorkers; i++) {
+            q.add (new thread(i));
+        }
+        for(int i = 0;i<jobs.length;i++){
+            int duration = jobs[i];
+            thread td = q.poll();
+            assignedWorker[i] = td.id;
+            startTime[i] = td.next_free_time;
+            td.next_free_time+=duration;
+            q.add(td);
+        }
+        /*for (int i = 0; i < jobs.length; i++) {
             int duration = jobs[i];
             int bestWorker = 0;
             for (int j = 0; j < numWorkers; ++j) {
@@ -45,7 +60,7 @@ public class JobQueue {
             assignedWorker[i] = bestWorker;
             startTime[i] = nextFreeTime[bestWorker];
             nextFreeTime[bestWorker] += duration;
-        }
+        }*/
     }
 
     public void solve() throws IOException {
@@ -77,4 +92,41 @@ public class JobQueue {
             return Integer.parseInt(next());
         }
     }
+    class thread{
+        public int id;
+        public long next_free_time;
+        thread(int id,long t ){
+            this.id = id;
+            this.next_free_time = t;
+        }
+        thread(int id){
+            this.id = id;
+            this.next_free_time = 0;
+        }
+        public void addtime(long t){
+            this.next_free_time += t;
+        }
+
+    }
+    Comparator<thread> lt = new Comparator<thread>() {
+        @Override
+        public int compare(thread thread, thread t1) {
+            if(thread.next_free_time == t1.next_free_time){
+                if(thread.id < t1.id)
+                    return -1;
+                else if(thread.id > t1.id)
+                    return 1;
+                else
+                    return 0;
+            }
+            else if(thread.next_free_time < t1.next_free_time)
+                return -1;
+            else if(thread.next_free_time > t1.next_free_time)
+                return 1;
+            else
+                return 0;
+
+        }
+    };
+
 }
